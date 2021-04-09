ba02cac9d15e5384177e6d905afb3d4eb335379d
jinwook-song <wlsdnr129@naver.com>
2021-04-09 23:11:57 +0900

    function

--- operator.js
+++ operator.js
@@ -34,8 +34,142 @@ x -= y;
 x *= y;
 x /= y;
 
-//6. Comparison operators
+// 5. Comparison operators
 console.log(10 < 6); //less than
 console.log(10 <= 6); //less than or equal
 console.log(10 > 6); //greater than
 console.log(10 >= 6); //greater than or equal
+
+// 6. Logical operators: || (or), && (and), ! (not)
+const value1 = true;
+const value2 = 4 < 2;
+
+// || (or)
+console.log(`or: ${value1 || value2 || check()}`);
+
+// && (and)
+console.log(`or: ${value1 && value2 && check()}`);
+
+// often used to compress long if-statement
+// nullableObject && nullableObject.somthing
+
+function check() {
+  for (let i = 0; i < 10; i++) {
+    // wasting time
+    console.log("❌");
+  }
+  return true;
+}
+
+// ! (not)
+console.log(!value1);
+
+// 7. Equality
+const stringFive = "5";
+const numberFIve = 5;
+
+// == loose equality, with type conversion
+console.log(stringFive == numberFIve);
+console.log(stringFive != numberFIve);
+
+// === strict equality, no type conversion
+console.log(stringFive === numberFIve);
+console.log(stringFive !== numberFIve);
+
+// object equality by reference
+const ellie1 = { name: "ellie" };
+const ellie2 = { name: "ellie" };
+const ellie3 = ellie1;
+console.log(ellie1 == ellie2);
+console.log(ellie1 === ellie2);
+console.log(ellie1 === ellie3);
+
+// equality - puzzler
+console.log(0 == false);
+console.log(0 === false);
+console.log("" == false);
+console.log("" === false);
+console.log(null == undefined);
+console.log(null === undefined);
+
+// 8. Conditional operators: if
+// if, else if, else
+const vName = "ellie";
+if (vName === "ellie") {
+  console.log("Welcom, Ellie!");
+} else if (vName === "coder") {
+  console.log("You are coder");
+} else {
+  console.log("unknown");
+}
+
+// 9. Ternary operator: ?
+// condition ? value1 : value2;
+console.log(vName === "ellie" ? "yes" : "no");
+
+// 10. Switch statement
+// use for muliple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = "IE";
+switch (browser) {
+  case "IE":
+    console.log("go away");
+    break;
+  case "Chrome":
+  case "Firefox":
+    console.log("love you");
+    break;
+  default:
+    console.log("same all");
+    break;
+}
+
+// 11. Loops
+// while loop, while the condition is truthy,
+// body code is executed.
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+// do while loop, body code is executed first,
+// then check the condition.
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+
+// for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+  console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+  // inline variable declaration
+  console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i: ${i}, j:${j}`);
+  }
+}
+
+// break, continue
+// 01. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(`q1. ${i}`);
+}
+// 02. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (let i = 0; i <= 10; i++) {
+  if (i > 8) {
+    break;
+  }
+  console.log(`q2. ${i}`);
+}
